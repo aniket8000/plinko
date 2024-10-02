dcb14d5314e0779a8cd36096f0195a083b25ca09
Aniket Kumar <aniketkumarvn@gmail.com>
2024-10-02 17:20:58 +0530

    commit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,35 +1,12 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { BallManager } from "../game/classes/BallManager";
 import { WIDTH } from "../game/constants";
 import { pad } from "../game/padding";
-import { useNavigate } from "react-router-dom";
 import { Simulate } from "../components/Simulate";
 import { Quotes, FoundIssue } from "../components";
 
 export function Home() {
-  const navigate = useNavigate();
-
   const canvasRef = useRef<any>();
-  let [outputs, setOutputs] = useState<{ [key: number]: number[] }>({
-    0: [],
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-    7: [],
-    8: [],
-    9: [],
-    10: [],
-    11: [],
-    12: [],
-    13: [],
-    14: [],
-    15: [],
-    16: [],
-    17: [],
-  });
 
   async function simulate(ballManager: BallManager) {
     let i = 0;
@@ -43,15 +20,7 @@ export function Home() {
   useEffect(() => {
     if (canvasRef.current) {
       const ballManager = new BallManager(
-        canvasRef.current as unknown as HTMLCanvasElement,
-        (index: number, startX?: number) => {
-          setOutputs((outputs: any) => {
-            return {
-              ...outputs,
-              [index]: [...(outputs[index] as number[]), startX],
-            };
-          });
-        }
+        canvasRef.current as unknown as HTMLCanvasElement
       );
       simulate(ballManager);
 
@@ -63,7 +32,7 @@ export function Home() {
 
   return (
     <div className="">
-      <div className="flex flex-col lg:flex-row  items-center justify-between ">
+      <div className="flex flex-col lg:flex-row items-center justify-between">
         <Simulate />
         <Quotes />
       </div>
