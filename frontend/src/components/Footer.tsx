4271a462168805f3b2cd1a3d3d5bcae9af988aa0
Aniket Kumar <aniketkumarvn@gmail.com>
2024-10-02 16:32:03 +0530

    plinko game

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+
+export const Footer = () => {
+  return (
+    <footer >
+      <div >
+        
+        <div>
+          
+        </div>
+      </div>
+    </footer>
+  );
+};
---
+++ frontend/src/components/FoundIssue.tsx
@@ -0,0 +1,10 @@
+export const FoundIssue = () => {
+  return (
+    <div >
+      <div >
+        
+        
+      </div>
+    </div>
+  );
+};
